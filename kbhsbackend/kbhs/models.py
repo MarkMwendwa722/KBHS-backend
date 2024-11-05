12a371c72fbf53b267ab5a7db832ad08e979a46a
Linah Ndichu <linahndichu7@gmail.com>
2024-11-05 10:25:25 +0300

    Changes

--- kbhsbackend/kbhs/models.py
+++ kbhsbackend/kbhs/models.py
@@ -4,15 +4,15 @@ from django.core.validators import EmailValidator
 
 # Create your models here.
 
-# class Assignment(models.Model):
-#     id = models.AutoField(primary_key=True)
-#     name = models.TextField(max_length=100, default="Unnamed Assignment")
-#     date = models.DateField(auto_now_add=True)
-#     form = models.CharField(max_length=40)
-#     file = models.FileField(upload_to='assignment/', default='assignment/default.pdf')
+class Assignment(models.Model):
+    id = models.AutoField(primary_key=True)
+    name = models.TextField(max_length=100, default="Unnamed Assignment")
+    date = models.DateField(auto_now_add=True)
+    form = models.CharField(max_length=40)
+    file = models.FileField(upload_to='assignment/', default='assignment/default.pdf')
     
-#     def __str__(self):
-#         return f"{self.id} - {self.file}"
+    def __str__(self):
+        return f"{self.id} - {self.file}"
     
 class Admissions(models.Model):
     name=models.CharField(max_length=100)
